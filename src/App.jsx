8a0b6442f190aa77b00c7fe43eb06868f35f6dc0
Sahil Dafade <sahildafade123@gmail.com>
2025-04-27 13:45:12 +0530

    create a project for ai image generation

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 
 import { useState } from 'react'
 import './App.css'
-import FileOutput from './component/FileOutput'
-import FileUploader from './component/FileUploader'
+import Generater from './component/Generater'
 
 function App() {
-  const [processedText,setProcessText]=useState('')
-  const [isLoading,setIsLoading] = useState(false);
+
 
   return (
 <div>
-  <div className='text-center font-semibold  mt-4 text-5xl space-y-3'>
-  <div>
-    Document Summarizer
-  </div>
-  <div className='text-lg font-light'>
-    Upload your PDF or Word document to get an AI-powered summary
-  </div>
-  </div>
-
-  <div>
-    <FileUploader setisLoading ={setIsLoading} onprocessedText={setProcessText}/>
-  </div>
-  <div>
-    <FileOutput isLoading={isLoading} processedText={processedText}/>
-  </div>
+<Generater/>
 </div>
   )
 }
---
+++ src/component/Generater.jsx
@@ -0,0 +1,104 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const Generater = () => {
+  const [prompt, setPrompt] = useState("");
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const generateImage = async () => {
+    setLoading(true);
+    const response = await fetch(
+      "https://router.huggingface.co/hf-inference/models/stabilityai/stable-diffusion-xl-base-1.0",
+      {
+        headers: {
+          Authorization: "Bearer ",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({
+          inputs: prompt,
+          options: {
+            wait_for_model: true,
+          },
+        }),
+      }
+    );
+    const blob = await response.blob();
+    const imageurl = URL.createObjectURL(blob);
+    setImage(imageurl);
+    setLoading(false);
+  };
+
+  return (
+    <section className="min-h-screen bg-gradient-to-br from-purple-900 via-black to-purple-700 overflow-y-auto">
+      <motion.div
+        initial={{ opacity: 0, y: 80 }}
+        animate={{ opacity: 1, y: 50 }}
+        className="max-w-2xl mx-auto p-6 bg-gradient-to-b from-white/20 to-purple-/10 backdrop-blur-md rounded-md shadow-2xl "
+      >
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="text-3xl font-semibold mb-6 text-white text-center"
+        >
+          AI Image Generater
+        </motion.h2>
+        <div>
+          <motion.input
+            whileFocus={{ scale: 1.03 }}
+            type="text"
+            value={prompt}
+            onChange={(e) => {
+              setPrompt(e.target.value);
+            }}
+            placeholder="Type Here To Generate Alike Image..."
+            className="w-full p-4 border border-purple-200 rounded-lg bg-white/10 text-white placeholder-purple-300 backdrop-blur-lg focus:outline-none
+            focus:border-purple-500 transition-all
+"
+          />
+        </div>
+        <motion.button
+          whileHover={{ scale: 1.03 }}
+          whileTap={{ scale: 1.08 }}
+          onClick={generateImage}
+          className="w-full bg-gradient-to-r border from-purple-600 to placeholder-purple-800 text-white rounded-md py-2 font-semibold mt-4"
+        >
+          {loading ? (
+            <div className="flex items-center justify-center">
+              <motion.div
+                animate={{ rotate: 360 }}
+                transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
+                className="w-6 h-6 border-2 border-white border-t-transparent rounded-full mr-2"
+              />
+              Generating...
+            </div>
+          ) : (
+            "Generate Image"
+          )}
+        </motion.button>
+        {image &&(
+            
+                <motion.div
+                initial={{ opacity: 0, y: 80 }}
+                animate={{ opacity: 1, y: 50 }}
+                className="mt-4"
+                >
+                    <motion.img
+                    src={image}
+                    alt="Generated AI Image"
+                    className="w-full rounded-lg shadow-2xl"
+                    />
+
+
+                    
+
+                    
+                </motion.div>
+        )}
+      </motion.div>
+    </section>
+  );
+};
+
+export default Generater;
